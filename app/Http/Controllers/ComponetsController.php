ff3499b110db10bd63b40776dd416e3928fae4df
dylan326 <dylanoldham326@gmail.com>
2019-12-06 14:45:55 -0500

    just need to style component-metadata view

--- app/Http/Controllers/ComponetsController.php
+++ app/Http/Controllers/ComponetsController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 use App\Timelog;
 use App\Componet;
 
@@ -11,11 +11,30 @@ class ComponetsController extends Controller
 {
     public function getTotalSecondsLogged()
     {
-        $totalSeconds = Timelog::join('issue_componets', 'issue_componets.issue_id', '=', 'timelogs.issue_id')
-        ->select(sum('timelogs.seconds_logged as totalSeconds'), 'issue_componets.componet_id as componet_id');
+        $totalSeconds = DB::table('timelogs')->join('issue_componets', 'timelogs.issue_id', '=', 'issue_componets.issue_id')->select(DB::raw('SUM(timelogs.seconds_logged) as total_seconds, issue_componets.componet_id'))->groupBy('componet_id')->get();
+        
 
         return $totalSeconds;
     }
+
+    public function usersComponents()
+    {
+        $userComponent = DB::table('timelogs')->join('issue_componets', 'timelogs.issue_id', '=', 'issue_componets.issue_id')
+        ->select(DB::raw('COUNT(timelogs.user_id) as numOfUsers, issue_componets.componet_id'))->groupBy('componet_id')->get();
+       // select COUNT(timelogs.user_id), issue_componets.componet_id from timelogs 
+       //inner join issue_componets on timelogs.issue_id = issue_componets.issue_id group by componet_id;
+
+       return $userComponent;
+    }
     
+
+    public function outputComponentMetaData()
+    {
+        $totalSeconds = $this->getTotalSecondsLogged();
+        $userComponent = $this->usersComponents();
+
+        return view('componets.index', ['totalSeconds' => $totalSeconds, 'userComponent' => $userComponent]);
+
+    }
   
 }
